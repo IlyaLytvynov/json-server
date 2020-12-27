83a57b1950f9818f07115c8b8494a600b031d4c2
Illial <illial@wix.com>
2020-12-27 16:23:22 +0200

    Added static files support

--- server.js
+++ server.js
@@ -1,30 +1,63 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const multer = require('multer');
+const middlewares = jsonServer.defaults();
+const path = require('path');
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  },
+});
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+server.use(jsonServer.bodyParser);
+server.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('avatar')
+);
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
-
-// Add custom routes before JSON Server router
+server.use(middlewares);
+// Add cusrtom routes before JSON Server router
 server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
-
+  res.jsonp(req.query);
+});
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
+    req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
+server.post('/comments', (req, res, next) => {
+  if (req.file && req.file.path) {
+    const [_, ...rest] = req.file.path.split('/');
+    req.body.avatarUrl = rest.join('/');
+  } else {
+    req.body.avatarUrl = 'images/default-avatar.png';
+  }
+
+  next();
+});
 
 // Use default router
-server.use(router)
-const port = process.env.PORT || 4000
+server.use(router);
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
-  console.log('JSON Server is running:' + port)
-})
+  console.log('JSON Server is running:' + port);
+});
